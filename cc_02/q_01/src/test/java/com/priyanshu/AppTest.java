19071d5b7e14a7b627f2e52f94c78a06a4e35b52
Priyanshu T <priyanshu9943@gmail.com>
2024-04-26 20:46:18 +0530

    cc_02 code updated

--- cc_02/q_01/src/test/java/com/priyanshu/AppTest.java
+++ cc_02/q_01/src/test/java/com/priyanshu/AppTest.java
@@ -20,6 +20,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -75,7 +76,7 @@ public class AppTest {
     public void setupReport() {
         logger.info("Setting up report...");
 
-        ExtentSparkReporter spark = new ExtentSparkReporter("./src/report/index.html");
+        ExtentSparkReporter spark = new ExtentSparkReporter("./out/report/index.html");
         spark.config().setDocumentTitle("Barnes & noble Testing Report");
         this.reports = new ExtentReports();
         this.reports.attachReporter(spark);
@@ -129,7 +130,7 @@ public class AppTest {
             logger.error("The keyword '" + searchValue + "' is not found");
         }
 
-        takeScreenshot("./src/screenshot/search");
+        takeScreenshot("./out/screenshot/search");
         logger.info("testSearchBar complete.");
     }
 
@@ -157,34 +158,82 @@ public class AppTest {
         logger.info("Scroll down and click the 'ADD TO CART' button.");
         WebElement addToCart = wait.until(d -> d.findElement(By.xpath(
                 "/html/body/main/div[2]/div[1]/div/div[2]/div/div[2]/section[2]/ol/li[1]/div/div[2]/div[5]/div[2]/div/div/form/input[11]")));
-        actions.scrollByAmount(0, 300);
+        actions.scrollByAmount(0, 450).perform();
+        actions.moveToElement(addToCart).perform();
         Thread.sleep(5000);
-        addToCart.click();
+        // addToCart.click();
+        actions.click(addToCart).perform();
 
         // Step 4
         logger.info("Verify the pop-up displays the label 'Item Successfully Added To Your Cart'.");
 
-        WebElement successLabel;
         ExtentTest verifySuccess = reports.createTest("Verify Add to Cart");
 
         try {
-            successLabel = wait.until(d -> d.findElement(By.cssSelector(".modal .item-add-msg")));
+            WebElement successLabel = wait.until(d -> d.findElement(By.cssSelector(".modal .item-add-msg")));
+
+            if (successLabel.getText().equals("Item Successfully Added To Your Cart")) {
+                verifySuccess.log(Status.PASS, "Item Successfully Added To Your Cart");
+                logger.info("Item Successfully Added To Your Cart");
+            } else {
+                verifySuccess.log(Status.FAIL, "Item failed to be Added To Your Cart");
+                logger.error("Item failed to be Added To Your Cart");
+            }
         } catch (Exception e) {
             verifySuccess.log(Status.FAIL, "Item failed to be Added To Your Cart");
             logger.error("Item failed to be Added To Your Cart. Because the modal did not appear.");
-            return;
         }
 
-        if (successLabel.getText().equals("Item Successfully Added To Your Cart")) {
-            verifySuccess.log(Status.PASS, "Item Successfully Added To Your Cart");
-            logger.info("Item Successfully Added To Your Cart");
-        } else {
-            verifySuccess.log(Status.FAIL, "Item failed to be Added To Your Cart");
-            logger.error("Item failed to be Added To Your Cart");
-        }
-
-        takeScreenshot("./src/screenshot/modal");
+        takeScreenshot("./out/screenshot/modal");
         logger.info("testAddToCart complete.");
+    }
+
+    /**
+     * Testcase 3
+     */
+
+    @Test(priority = 3)
+    public void testMembershipPage() throws Exception {
+        logger.info("Starting testMembershipPage...");
+
+        // Step 1
+        driver.get("https://www.barnesandnoble.com/");
+
+        // Step 2
+        logger.info("Scroll down and click on 'B&N MEMBERSHIP.'");
+        WebElement membership = wait.until(d -> d.findElement(By.linkText("B&N MEMBERSHIP")));
+        actions.scrollByAmount(0, 500).perform();
+        membership.click();
+
+        // Step 3
+        logger.info("Scroll down to the last and, under the label 'FREE,' click on the 'JOIN REWARDS' button.");
+        WebElement joinRewards = wait.until(d -> d.findElement(By.linkText("JOIN REWARDS")));
+        actions.scrollToElement(joinRewards).perform();
+        actions.scrollByAmount(0, 100).perform();
+        joinRewards.click();
+
+        // Step 4
+        logger.info("Verify the pop-up has label 'Sign in or Create an Account'.");
+        logger.info("Switching to modal frame...");
+        WebElement modalFrame = wait.until(d -> d.findElement(By.cssSelector("iframe[data-membershipflag]")));
+        driver.switchTo().frame(modalFrame);
+
+        WebElement signInLabel = wait.until(d -> d.findElement(By.className("modal__header")));
+        ExtentTest verifySignIn = reports.createTest("Verify Sign In");
+
+        if (signInLabel.getText().equals("Sign in or Create an Account")) {
+            verifySignIn.log(Status.PASS, "Sign in or Create an Account");
+            logger.info("Sign in or Create an Account");
+        } else {
+            verifySignIn.log(Status.FAIL, "Sign in or Create an Account");
+            logger.error("Sign in or Create an Account");
+        }
+
+        logger.info("Switching back to default frame...");
+        driver.switchTo().defaultContent();
+
+        takeScreenshot("./out/screenshot/membership");
+        logger.info("testMembershipPage complete.");
     }
 
     @AfterTest
@@ -198,8 +247,7 @@ public class AppTest {
     private void takeScreenshot(String filename) throws IOException {
         logger.info("Taking screenshot of " + filename);
         File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-        FileUtils.copyFile(screenshotFile, new File(filename + "_" + timestamp + ".png"));
+        FileUtils.copyFile(screenshotFile, new File(filename + ".png"));
         logger.info("Screenshot taken.");
     }
 }
