fc5fbcbdd7a05cf23f7b073a26a5262f8812b4ba
Priyanshu T <priyanshu9943@gmail.com>
2024-05-23 09:20:18 +0530

    Delete unused files and update application.properties

--- projects/skcet-automation/pod-automation/src/main/java/com/priyanshu/automation/secret/Secret.java
+++ projects/skcet-automation/pod-automation/src/main/java/com/priyanshu/automation/secret/Secret.java
@@ -1,5 +1,11 @@
 package com.priyanshu.automation.secret;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Secret {
-    public static String password = "priyanshu#1";
+    @Value("${email}")
+    public static String email;
+
+    @Value("${password}")
+    public static String password;
 }
